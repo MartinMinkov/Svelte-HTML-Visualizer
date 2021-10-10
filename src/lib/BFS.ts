351e6090d5ebbdd9d0447e87d949429ac13fe0e1
Martin Minkov <minkovlmartin@gmail.com>
2021-10-10 14:53:25 -0700

    fix naming in DFS

--- src/lib/BFS.ts
+++ src/lib/BFS.ts
@@ -1,14 +1,14 @@
 export const traverseBFS = (root: HTMLElement, cb: (node: HTMLElement) => void) => {
-	let stack = Array.from(root.children);
+	let queue = Array.from(root.children);
 	let res: HTMLElement[] = [];
 
-	while (stack.length) {
-		const current = stack.shift() as HTMLElement;
+	while (queue.length) {
+		const current = queue.shift() as HTMLElement;
 		res.push(current);
 
 		// Add children to the stack so we can further traverse their children
 		Array.from(current.children).forEach((child) => {
-			stack.push(child);
+			queue.push(child);
 		});
 		cb(current);
 	}
